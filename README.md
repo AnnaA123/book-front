e5180ae64088a63951d446f3ccad55ebdb850725
Anna <annamary.alava@gmail.com>
2021-05-03 20:38:44 +0300

    update

--- README.md
+++ README.md
@@ -6,9 +6,16 @@ Backend repository: https://github.com/AnnaA123/book-site/
 
 Book data from [Google Books API](https://developers.google.com/books)
 
+---
 
+# npm packages
 
-===
+- react
+- react-router-dom
+- bootstrap
+
+React
+---
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/components/ListReviews.js
+++ src/components/ListReviews.js
@@ -10,9 +10,18 @@ import { getSingleUser } from '../util/UsersAPI';
         this.state = {
             reviews: [],
             users: [],
+            showContent: true,
             loading: true,
         }
+        this.handleChange = this.handleChange.bind(this);
         this.getReviews = this.getReviews.bind(this);
+    }
+
+    handleChange(event) {   
+        
+        this.setState((prev) => ({
+            showContent: !prev.showContent
+        }));
     }
 
     getBookId = () => {
@@ -29,7 +38,12 @@ import { getSingleUser } from '../util/UsersAPI';
               loading: false,
             });
           });
-        
+     }
+
+     getContent(review) {
+         if (this.state.showContent) {
+             return <p>{ review.Content }</p>
+         }
      }
 
     componentDidMount() {
@@ -44,15 +58,21 @@ import { getSingleUser } from '../util/UsersAPI';
                     </div>
         } else {
             if (this.state.reviews[0] !== undefined) {
-                return <div className="p-5 w-100 flex-column">{this.state.reviews.map((review) => {
+                return <div>
+                    <div className="mt-3">
+                        <input className="me-1" type="checkbox" id="sc" name="showContent" value={this.state.value} onClick={this.handleChange}/>
+                        <label for="sc">Hide full reviews</label><br/>
+                    </div>
+                    <div className="p-5 w-100 flex-column">{this.state.reviews.map((review) => {
                     return <div className="mb-3 border-bottom border-dark" key={review.id}>
                     <div className="mb-3 ">
-                        <Link className="link-danger text-decoration-none" to={`/review/${review.id}`}><h3>{ review.Title }</h3></Link>
-                        <p>{review.Rating}/5</p>
-                        <p>{ review.Content }</p>
+                        <div className="row">
+                            <Link className="link-danger text-decoration-none" to={`/review/${review.id}`}><h3>{ review.Title } <small class="text-muted">{review.Rating}/5</small></h3></Link>
+                        </div>
+                        { this.getContent(review) }
                         <Link className="link-dark fw-bold" to={`/users/${review.UserID.id}`}>{ review.UserID.username }</Link>
                     </div></div>
-                })}</div>
+                })}</div></div>
             } else {
                 return <div>
                         <p>No reviews</p>
